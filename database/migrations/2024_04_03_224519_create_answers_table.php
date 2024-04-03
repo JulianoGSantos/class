edc0a05299771f0ef9107f6a024987370b35c255
juliano <jot.asa@hotmail.com>
2024-04-03 20:54:40 -0300

    one_to_many

---
+++ database/migrations/2024_04_03_224519_create_answers_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('users_id')->constrained()->onDelete('cascade');
+            $table->string('first');
+            $table->string('second');
+            $table->string('third');
+            $table->string('fourth');
+            $table->string('fifth');
+            $table->string('sixth');
+            $table->string('seventh');
+            $table->string('eighth');
+            $table->string('ninth');
+            $table->string('tenth');
+            $table->string('eleventh');
+            $table->string('twelfth');
+            $table->string('thirteenth');
+            $table->string('fourteenth');
+            $table->string('fifteenth');
+            $table->string('sixteenth');
+            $table->string('seventeenth');
+            $table->string('eighteenth');
+            $table->string('nineteenth');
+            $table->string('twentieth');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('answers');
+    }
+};
---
+++ resources/views/site/index.blade.php
@@ -0,0 +1,33 @@
+@include('layouts.style')
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Teste</title>
+</head>
+<body>
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+          <div class="navbar-brand">
+            <img src="img/owl.png" alt="coruja" width="70" height="50" class="d-inline-block align-text-top">
+            Simulado
+          </div>
+        </div>
+    </nav>
+    <div class="form-check">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1">
+        <label class="form-check-label" for="flexRadioDefault1">
+          2/6
+        </label>
+      </div>
+      <div class="form-check">
+        <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2">
+        <label class="form-check-label" for="flexRadioDefault2">
+          3/6
+        </label>
+      </div>
+</body>
+</html>
